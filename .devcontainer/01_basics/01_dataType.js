7714cabaa8e27da8bf9cb81921fda67c5db35363
Chandani Raj <chandaniggpa@gmail.com>
2024-06-27 18:23:02 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_dataType.js
@@ -0,0 +1,15 @@
+let name="chandani"
+let age=21
+let isLoggedIn=true
+//number => 2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+//object
+console.log(typeof"chandani")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
--- .devcontainer/01_basics/01_variables.js
+++ .devcontainer/01_basics/01_variables.js
@@ -1,6 +1,14 @@
-const accountId=15732
-let accountEmail="chaandaniggpa@gmail.com"
-var accountPassword="1234"
-accountCity="patna"
-accountId=2
-console.log("accountId")
+const accountId = 15732
+let accountEmail = "chaandaniggpa@gmail.com"
+var accountPassword = "1234"
+accountCity = "patna"
+let accountState;
+//accountId = 2//not allowed
+accountEmail="ch@gmail.com"
+accountPassword="29792"
+accountCity="sasaram"
+console.log(accountId);
+/* 
+prefer to not use var because of issues in block scope and functional scope
+*/
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
