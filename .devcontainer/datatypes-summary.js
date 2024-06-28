1650c7b273eb33b3eb8d046d9a41eee473518c82
Chandani Raj <chandaniggpa@gmail.com>
2024-06-28 13:47:41 +0000

    dataype-summary

---
+++ .devcontainer/datatypes-summary.js
@@ -0,0 +1,17 @@
+//primitive
+
+// 7 types: string, NUmber,Boolean,Null,Undefined,symbol,BigInt
+
+const score = 100
+const scorevalue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+const id = symbol('123')
+const anotherId = symbol('123')
+console.log(id === anotherId)
+
+
+//Reference(non primitive)
+
+//Array,object,Functions
