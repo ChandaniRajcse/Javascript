b616563f13acd218dba0a4503ab2f1a32fa30050
Chandani Raj <chandaniggpa@gmail.com>
2024-06-28 13:47:10 +0000

    ConversionOperation

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -1,2 +1,12 @@
-let score=33
+let score="33"
 console.log(typeof score)
+console.log(typeof (score))
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+//operation
+let str1="Hello"
+let str2=" chandani"
+let str3= str1+str2
+console.log(str3)
+console.log("1"+2)
+console.log(1+2+"3")
