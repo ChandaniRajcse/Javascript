09cd06988ad2e9cb9dc0ab6f8c2192846de6ee33
Chandani Raj <chandaniggpa@gmail.com>
2024-07-02 19:29:28 +0000

    math and num.js

---
+++ .devcontainer/01_basics/06_nums_and_math.js
@@ -0,0 +1,35 @@
+const score = 400
+//console.log(score)
+ 
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(1));
+
+const othername = 123.8966
+
+//console.log(othername.toPrecision(4));
+
+const hundreds = 10000000
+//console.log(hundreds.toLocaleString());
+
+
+//++++++++++++++++++++++++ MATHS +++++++++++++++
+
+console.log(Math);
+console.log(Math.round(3.5));
+console.log(Math.abs(-4));
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4.9));
+console.log(Math.min(1,2,3,4,5));
+console.log(Math.max(1,23,4,5));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+
+
+
+
+
+
